5e67c748e77f088836061047d136c5f7141b48a6
maxefimov <efimov.top.academy@gmail.com>
2023-03-23 19:01:20 +0300

    23-03-23

--- TagHelpers/SortHeaderTagHelper.cs
+++ TagHelpers/SortHeaderTagHelper.cs
@@ -1,7 +1,9 @@
 ﻿using Microsoft.AspNetCore.Razor.TagHelpers;
 using Microsoft.AspNetCore.Mvc.Rendering;
 using MvcEFApp.Models;
-
+using Microsoft.AspNetCore.Mvc.ViewFeatures;
+using Microsoft.AspNetCore.Mvc.Routing;
+using Microsoft.AspNetCore.Mvc;
 
 namespace MvcEFApp.TagHelpers
 {
@@ -11,5 +13,36 @@ namespace MvcEFApp.TagHelpers
         public SortProp Current { get; set; }
         public string? Action { get; set; } 
         public bool Up { get; set; }
+
+        [ViewContext]
+        [HtmlAttributeNotBound]
+        public ViewContext ViewContext { get; set; } = null!;
+
+        IUrlHelperFactory urlHelperFactory;
+        public SortHeaderTagHelper(IUrlHelperFactory helperFactory)
+        {
+            urlHelperFactory = helperFactory;
+        }
+
+        public override void Process(TagHelperContext context, TagHelperOutput output)
+        {
+            IUrlHelper urlHelper = urlHelperFactory.GetUrlHelper(ViewContext);
+            output.TagName = "a";
+            string? url = urlHelper.Action(Action, new { sortOrder = Property });
+            output.Attributes.SetAttribute("href", url);
+            // если текущее свойство имеет значение CurrentSort
+            if (Current == Property)
+            {
+                TagBuilder tag = new TagBuilder("i");
+                tag.AddCssClass("glyphicon");
+
+                if (Up == true)   // если сортировка по возрастанию
+                    tag.AddCssClass("glyphicon-chevron-up");
+                else   // если сортировка по убыванию
+                    tag.AddCssClass("glyphicon-chevron-down");
+
+                output.PreContent.AppendHtml(tag);
+            }
+        }
     }
 }
