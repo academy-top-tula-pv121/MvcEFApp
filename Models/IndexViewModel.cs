dd760b1dc8b43f80a8ace147fd5c7b98ac698fd2
maxefimov <efimov.top.academy@gmail.com>
2023-03-23 19:20:48 +0300

    23-03-23

---
+++ Models/IndexViewModel.cs
@@ -0,0 +1,10 @@
+﻿namespace MvcEFApp.Models
+{
+    public class IndexViewModel
+    {
+        public IEnumerable<Employee> Employees { get; set; } 
+            = new List<Employee>();
+        public SortViewModel SortViewModel { set; get; } 
+            = new SortViewModel(SortProp.NameAsc);
+    }
+}
